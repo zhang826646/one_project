b79c029957d3cb69a6b327bf29aeb9bf10084f7d
zhang <zhang@123.com>
2021-12-22 10:42:28 +0800

    文章

--- apps/web_api/view/group.py
+++ apps/web_api/view/group.py
@@ -29,6 +29,39 @@ async def index(request):
     return data
     # return json(data)
 
+
+@doc.summary('主页1')
+@doc.produces({
+    'code': doc.Integer('状态码'),
+    'msg' : doc.String('消息提示'),
+    'data': {'token': doc.String('Token')}
+}, content_type='application/json', description='Request True')
+async def index1(request):
+    ttm_sql = request.app.ttm.get_mysql('ttm_sql')
+
+    @run_sqlalchemy()
+    def get_circles(db_session):
+        return db_session.query(CirclePost).filter().order_by(CirclePost.created_at.desc()).limit(20).all()
+
+    item = []
+    for row in await get_circles(ttm_sql):
+        item.append({
+            'id': row.id,
+            'catalog_id': row.catalog_id,
+            'type': row.type,
+            'title': row.title,
+            'content': row.content,
+            'total_comments': row.total_comments,
+            'attachments': row.attachments,
+            'created_at': row.created_at,
+
+        })
+    return json({
+        'code': 0,
+        'msg': '',
+        'data': item
+    })
+
 @doc.summary('查看文章详情')
 @doc.consumes(
     doc.JsonBody({
@@ -120,17 +153,107 @@ async def cat_post(request,post_id):
     return json({'code': ApiCode.SUCCESS, 'data': data})
 
 
-# @doc.summary('发布文章')
-# @doc.produces({
-#     'code': doc.Integer('状态码'),
-#     'msg' : doc.String('消息提示'),
-#     'data': {'token': doc.String('Token')}
-# }, content_type='application/json', description='Request True')
-# @mako.template('index.html')
-# async def index(request):
-#
-#     new_time=datetime.datetime.fromtimestamp(1636366351)
-#     data={'data':[{'title':'zhang','created_at':new_time,'excerpt':'这只是测试的一条数据','url':'http://www.sssoou.com','tags':[{'name':'技术','url':'https://www.baidu.com'}]},{'title':'zhang','created_at':new_time,'excerpt':'这只是测试的一条数据','url':'http://www.sssoou.com','tags':[{'name':'技术','url':'https://www.baidu.com'}]}]}
-#     # data={'users':[{'name':'user1'},{'name':'user2'},{'name':'user3'},{'name':'user4'},{'name':'user5'},{'name':'user6'}]}
-#     return data
-#     # return json(data)
+@doc.summary('编辑帖子')
+@doc.consumes(
+    doc.JsonBody({
+        'post_id'         : doc.Integer('帖子ID'),
+        'title'           : doc.String('帖子标题'),
+        'type'            : doc.Integer('类型[1技术|2生活]'),
+        'tag'             : doc.String('标签'),
+        'uid'             : doc.Integer('用户ID'),
+        'content_compiled': doc.String('帖子内容 HTML'),
+        'catalog_id'      : doc.Integer('版块ID'),
+
+    }), content_type='application/json', location='body', required=True
+)
+@doc.produces({
+    'code': doc.Integer('状态码'),
+    'msg' : doc.String('消息提示'),
+}, content_type='application/json', description='Request True')
+async def save_post(request):
+    post_id = request.valid_data.get('post_id')
+    title = request.valid_data.get('title','测试数据')
+    post_type = request.valid_data.get('type',1)
+    tag = request.valid_data.get('tag','测试')
+    uid = request.valid_data.get('uid',1000000)
+    catalog_id = request.valid_data.get('catalog_id',1)
+
+    ttm_sql = request.app.ttm.get_mysql('ttm_sql')
+
+    # group_catalogs = await get_group_catalogs(request.app)  #获取板块
+    atta_key = []
+
+    # async def _decompile_content(compiled):
+    #     """
+    #     将帖子内容 HTML <img> 标签  反编译为图片字段与内容字段
+    #     :param compiled:
+    #     :return:
+    #     """
+    #     attachment_no = 1
+    #     atta_list = []
+    #
+    #     def _repl(match):
+    #         nonlocal attachment_no
+    #         text = match.group(0)
+    #         data_attributes = {x: y for (x, y) in re.findall(r'(data-[^=]+)="([^"]+)"', text)}
+    #
+    #         if 'data-key' in data_attributes and 'data-mime-type' in data_attributes and \
+    #                 'data-width' in data_attributes and 'data-height' in data_attributes:
+    #             # 有data-xxx 字段直接取出返回
+    #             key = data_attributes['data-key']
+    #             mime_type = data_attributes['data-mime-type']
+    #             width = data_attributes['data-width']
+    #             height = data_attributes['data-height']
+    #             atta_list.append(f'p{attachment_no}#{key}#{mime_type}#{width}#{height}')
+    #             atta_key.append(key)
+    #             replacement = '{{p%s}}' % attachment_no
+    #             attachment_no = attachment_no + 1
+    #             return replacement
+    #         else:
+    #             # 没有 data-xxx 字段 取<img> src标签 上传到七牛云并获取宽、高、类型记录
+    #             src_match = re.search(r'src="([^"]+)"', text)
+    #             if not src_match:
+    #                 return ''
+    #             url = src_match.group(1)
+    #             url = re.sub(r'\?.*', '', url)
+    #
+    #             fetch_result = qiniu_helper.sync_fetch(url, path=f'attachment/{datetime.now().strftime("%Y/%m/%d/")}')
+    #
+    #             if fetch_result:
+    #                 key = fetch_result['key']
+    #                 mime_type = fetch_result['mimeType']
+    #                 composed_url = f'{cdn_url}{key}?imageInfo'
+    #                 res = requests.get(composed_url)
+    #                 if res.status_code == 200:
+    #                     width = res.json()['width']
+    #                     height = res.json()['height']
+    #                     atta_list.append(f'p{attachment_no}#{key}#{mime_type}#{width}#{height}')
+    #                     replacement = '{{p%s}}' % attachment_no
+    #                     attachment_no = attachment_no + 1
+    #                     return replacement
+    #                 else:
+    #                     return ''
+    #     contents = re.sub(r'<img[^>]+>', _repl, compiled)
+    #     return lxml.html.fromstring(contents).text_content(), '|'.join(atta_list), len(atta_list)
+    # content, attachments, total_attachments = await _decompile_content(content_compiled)
+    content='这是测试数据-内容，这是测试数据-内容，这是测试数据-内容，这是测试数据-内容，这是测试数据-内容，'
+    attachments=''
+    if not post_id:
+        item = CirclePost()
+        item.created_at = item.last_replied_at = now()
+        item.type = 0   # [0:普通帖子|1:互动贴]
+        ttm_sql.add(item)
+    else:
+        item = ttm_sql.query(CirclePost).filter(CirclePost.id == post_id).first()
+    item.title = title
+    item.type = post_type
+    item.content = content
+    item.tag = tag
+    item.uid = uid
+    item.attachments = attachments
+    item.catalog_id = catalog_id
+    # ttm_sql.flush()
+    ttm_sql.commit()
+
+
+    return json({'code': ApiCode.SUCCESS, 'msg': '操作成功', 'data': {'id': item.id}})
--- common/dao/circle.py
+++ common/dao/circle.py
@@ -40,3 +40,25 @@ class CircleComment(BaseModel):
     total_replies = Column(Integer, default=0, comment='子回复数量')
     total_attachments = Column(SmallInteger, default=0, comment='图片数量')
     attachments = Column(Text(), nullable=True, default='', comment='图片')
+
+
+class CircleCatalog(BaseModel):
+    __tablename__ = '_circle_catalog'
+
+    id = Column(Integer, primary_key=True)
+    catalog_name = Column(Integer, comment='板块名称')
+    uid = Column(Integer, comment='用户ID')
+    type = Column(SmallInteger, default=0, comment='类型[1技术|2生活]')
+    title = Column(String(50), default='', comment='帖子标题')
+    content = Column(Text(), nullable=True, default='', comment='帖子内容')
+    tag = Column(String(50), nullable=True, default='', comment='标签')
+    deleted = Column(SmallInteger, default=0, comment='删除[0:正常|1:删除]')
+    locked = Column(SmallInteger, default=0, comment='锁定(禁止回复)[0:正常|1:锁定]')
+    picked = Column(SmallInteger, default=0, comment='精选[0:正常|1:精选]')
+    hidden = Column(SmallInteger, default=0, comment='隐藏[0:正常|1:隐藏]')
+    total_comments = Column(SmallInteger, default=0, comment='评论总数')
+    total_floors = Column(SmallInteger, default=1, comment='楼层总数')
+    total_attachments = Column(SmallInteger, default=0, comment='图片总数')
+    attachments = Column(Text(), nullable=True, default='', comment='图片')
+    last_replied_at = Column(Integer, default=0, comment='最近回复时间')
+    created_at = Column(Integer, default=now, comment='创建时间')
